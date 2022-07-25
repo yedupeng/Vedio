b9ce3194ee8009c017c41a0d37c383fd76aada1a
lolikonloli <183426023@qq.com>
2022-07-25 02:28:47 +0000

    优化测试摄像头显示

--- test/camera.py
+++ test/camera.py
@@ -1,12 +1,13 @@
 import cv2
 from loguru import logger
-import loguru
 
 for i in range(4):
-    for i in range(10):
+    for j in range(10):
         cap = cv2.VideoCapture(i)
         ret, frame = cap.read()
         if ret:
-            logger.info('camera:{} is OK! shape is {}'.format(i, frame.shape))
+            logger.debug('camera:{} is OK! shape is {}'.format(i, frame.shape))
+            cap.release()
             break
         logger.warning('cant open camera:{} !'.format(i))
+        break
